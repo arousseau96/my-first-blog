a69aab824f65259b7bdf7f8d76caaf2316d82ab8
Antoine Rousseau <itoute@hotmail.com>
2019-03-04 20:59:40 -0500

    Modification du HTML du site

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'fudyfy4t(e6^wx!1(3l$m1(qfn&_4g768h7%(4uqpwt=n2pkr4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['198.211.99.20', 'localhost', '127.0.0.1']
 
 
 # Application definition
